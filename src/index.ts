01b9ebb5b106b4715241a608e49dec9bac8d6669
unknown <muktinathrajbanshi786@gmail.com>
2026-02-20 09:52:31 +0545

    lage raho munna bhai

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-console.log("Hello, Muktinath!");
-console.log("Are you alright");
+const getUsername = document.querySelector("#user") as HTMLInputElement;
+const formSubmit = document.querySelector(".form") as HTMLFormElement;
+const main_container = document.querySelector(".main-container") as HTMLElement;
+
+// so lets define the contract of an object 
+interface UserData {
+    id : number;
+    login : string;
+    avatar_url : string;
+    location : string;
+    url : string;
+}
+
+// reusable function 
+function myCustomFetcher<T>(url : string, options? : RequestInit) : Promise<T> {
+    
+}
+
+function fetchUserData(url : string) {
+    myCustomFetcher(url, {});
+}
+
+
+// default function call 
+fetchUserData("https://api.github.com/users");
